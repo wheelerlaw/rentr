f2c4cfd362b26c4bc896b0a58ba8a20d7f8e20d4
Wheeler Law <whelderwheels613@gmail.com>
2018-02-03 01:05:37 -0500

    Adde view for utilities

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('utility/<int:utility_id>/', views.utility, name='utility'),
+    path('month/<str:year_month>/', views.utility, name='utility')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!")
+    utilities = Utility.objects.order_by('name')
+    context = {'utilities': utilities}
+    return render(request, 'index.html', context)
+
+
+def utility(request, utility_id):
+    utility = get_object_or_404(Utility, pk=utility_id)
+    bills = utility.bill_set.order_by('end_date')
+    return render(request, 'utility.html', {'utility': utility, 'bills': bills})
+
+
+def month(request):
+    return
